c2d24c5eda3bcf719a1b812ad19dc27ab4f4b937
Yohann Assouline <yohannassouline@hotmail.fr>
2026-06-18 14:48:59 +0200

    add astro template for site

---
+++ website/astro.config.mjs
@@ -0,0 +1,27 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+import starlight from '@astrojs/starlight';
+
+// https://astro.build/config
+export default defineConfig({
+	integrations: [
+		starlight({
+			title: 'FencingDIY',
+            // logo: { src: '../logos/logo-full.png', alt: 'logo'},
+			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/yohannfra/fencingdiy' }],
+			sidebar: [
+				{
+					label: 'Guides',
+					items: [
+						// Each item here is one entry in the navigation menu.
+						{ label: 'Example Guide', slug: 'guides/example' },
+					],
+				},
+				{
+					label: 'Reference',
+					items: [{ autogenerate: { directory: 'reference' } }],
+				},
+			],
+		}),
+	],
+});
