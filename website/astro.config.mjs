5faf40d637b37c0d798a028f294b5b1104778bfb
Yohann Assouline <yohannassouline@hotmail.fr>
2026-06-18 15:13:28 +0200

    fix website url

--- website/astro.config.mjs
+++ website/astro.config.mjs
@@ -4,7 +4,8 @@ import starlight from '@astrojs/starlight';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://fencingdiy.com',
+    site: 'https://yohannfra.github.io/fencing-diy-tmp/',
+    // site: 'https://fencingdiy.com',
     base: '/fencingdiy',
 	integrations: [
 		starlight({
